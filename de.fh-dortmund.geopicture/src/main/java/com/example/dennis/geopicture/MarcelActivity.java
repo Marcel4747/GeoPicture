2f3fef8c9da2f7526854ef8bca7221a8067301b2
Marcel4747 <marcel.pauck001@stud.fh-dortmund.de>
2015-04-20 10:48:22 +0200

    Einbinden der API!

--- de.fh-dortmund.geopicture/src/main/java/com/example/dennis/geopicture/MarcelActivity.java
+++ de.fh-dortmund.geopicture/src/main/java/com/example/dennis/geopicture/MarcelActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import de.do1900.persistence.data.*;
 
 public class MarcelActivity extends Activity {
 
@@ -12,7 +13,7 @@ public class MarcelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marcel);
-
+    Location location;
 
     }
 }
